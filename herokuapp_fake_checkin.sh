2a432484169d3741b1e3bd71f11477adfc2ad43e
Greg D. Hill <greghill@cs.stanford.edu>
2016-06-09 11:54:23 -0700

    post scripts

--- herokuapp_fake_checkin.sh
+++ herokuapp_fake_checkin.sh
@@ -1,1 +1,1 @@
-curl -i -X POST -d "hostname=test&datetime=`date --iso-8601=minutes`&payload=helooooworld" https://network-observatory.herokuapp.com/post-measurement-box-checkin
+curl -i -X POST -d "hostname=test&datetime=`date --iso-8601=minutes`&head=fakehead&temp=900.1" https://network-observatory.herokuapp.com/post-measurement-box-checkin
--- localhost_fake_checkin.sh
+++ localhost_fake_checkin.sh
@@ -1,1 +1,1 @@
-curl -i -X POST -d "hostname=test&datetime=`date --iso-8601=minutes`&payload=helooooworld" http://localhost:5000/post-measurement-box-checkin
+curl -i -X POST -d "hostname=test&datetime=`date --iso-8601=minutes`&head=fakehead&temp=900.1" http://localhost:5000/post-measurement-box-checkin
