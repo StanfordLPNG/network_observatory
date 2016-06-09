9a5dd5da55cc4db75d85ef01a043e738383a3c0d
Greg D. Hill <greghill@cs.stanford.edu>
2016-06-08 22:36:21 -0700

    change schema

--- measurement_box_monitor/models.py
+++ measurement_box_monitor/models.py
@@ -2,5 +2,6 @@ from django.db import models
 
 # Create your models here.
 class MeasurementBoxCheckin(models.Model):
+    hostname = models.CharField(max_length=256)
     datetime = models.DateTimeField('checkin time')
-    choice_text = models.CharField(max_length=200)
+    choice_text = models.CharField(max_length=400)
