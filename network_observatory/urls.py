ced09bb058b4fa94dd2d6996339fac520d45213f
Greg D. Hill <greghill@cs.stanford.edu>
2016-06-08 21:49:28 -0700

    hello world page

--- network_observatory/urls.py
+++ network_observatory/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+import measurement_box_monitor.views
+
 urlpatterns = [
+    url(r'^$', measurement_box_monitor.views.index, name='index'),
     url(r'^admin/', include(admin.site.urls)),
 ]
