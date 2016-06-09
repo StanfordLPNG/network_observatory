dc6b1aefb1d5d3b13d3a372a43cebfc15cd0871e
Greg D. Hill <greghill@cs.stanford.edu>
2016-06-08 22:17:28 -0700

    diplay from database

--- measurement_box_monitor/views.py
+++ measurement_box_monitor/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from .models import MeasurementBoxCheckin
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello world. We made it.")
+
+    checkins = MeasurementBoxCheckin.objects.all()
+
+    return render(request, 'checkin.html', {'checkins': checkins })
+    #return HttpResponse("Hello world. We made it.")
