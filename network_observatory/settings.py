aeb370e3aabd06d78545251248e4cd4a04448bfe
Greg D. Hill <greghill@cs.stanford.edu>
2016-06-09 17:26:29 -0700

    change to psql for local database

--- network_observatory/settings.py
+++ network_observatory/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'network_observatory.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'network_observatory',
+        'USER': 'local',
+        'PASSWORD': 'local',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
